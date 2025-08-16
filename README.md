544163527de14dfd11baec9038da6c07fca65047
Andreas Salcedo <andreas0salcedo@gmail.com>
2025-08-16 02:44:29 +0100

    Posts can now be made from client to flask server

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # publsh - Microblogging for the terminal
 
-Client CLI that works with publsh-server.
+Client CLI tool for [publsh-server](https://github.com/salcedoa/cli-blg).
+
+Set up on Linux/MacOS
+```
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip setuptools wheel
+pip install -r requirements.txt
+pip install -e .
+```
+
+Config file set up:
+'''
+touch ~/.config/publsh/publsh.yaml
+'''
+Inside the config file (with example URL):
+```
+api_url: "http://127.0.0.1"
+port: 5000
+```
--- publsh/controller.py
+++ publsh/controller.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import json
 import yaml
 import requests
+from urllib.parse import urljoin
 
 CONFIG_FILE = os.path.expanduser("~/.config/publsh/publsh.yaml")
 def load_config():
@@ -25,15 +26,18 @@ def create_post(post_input):
 
 def send_post(post):
     cfg = load_config()
-    url = cfg.get("api_url", "http://localhost")
+    base_url = f"{cfg.get('api_url')}:{cfg.get('port')}"
+    url = urljoin(base_url, "json")
+    print("Sending to",url)
     if post["title"] != None:
       try:
-        requests.post(url + "/json", json=post)
+        requests.post(url, json=post)
+        return "Post uploaded!"
       except requests.exceptions.RequestException as e:
+         print("Nothing happened")
          return e
     else:
        return "Post must not be empty"
-    return "Post uploaded!"
 
 
 def edit_post(post_id):
